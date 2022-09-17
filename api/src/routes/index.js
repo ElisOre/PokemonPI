a0a0812204fb4e9307a97e868f7bdcec1c215c7d
ElisOre <elias.orellana.31@gmail.com>
2022-09-17 04:16:24 -0300

    Rutas del back creadas, pero sin completar

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -8,5 +8,47 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+// Obtener un listado de pokemons
+router.get('/pokemons', (req, res) => {
+
+    // hacer un llamado asincronico a la api
+    // buscar qué me sirve y quedarme con eso
+    // validar que haya información y enviarla
+
+});
+
+// Obtener detalles de un pokemon por id
+router.get('/pokemons/:id', (req, res) => {
+
+    // llamado asinc por id (findByPK)
+    // verificar qué tipo de ID es (de la api o de la db)
+    // enviar los detalles
+
+});
+
+// Obtener pokemon que coincida con el nombre exacto (de pokeapi o creado)
+router.get('/pokemons', (req, res) => {
+
+    // validar si coincide lo que pasa por query (puede ser de la api o de la db)
+    // enviar el resultado correspondiente (mensaje de error u objeto json)
+
+});
+
+// Crear un pokemon con su tipo
+router.post('/pokemons', (req, res) => {
+
+    // verificar los datos por body a través del formulario
+    // relacionar el pokemon con sus tipos
+    // guardar en la db
+
+});
+
+// Obtener los tipos de pokemons
+router.get('/types', (req, res) => {
+
+    // llamado async a la api
+    // guardar el objeto en la db (verificar los datos)
+
+});
 
 module.exports = router;
