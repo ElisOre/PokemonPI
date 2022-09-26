10119d4bb1c5f13c4e2f1a3e8feef5ffaa37f2f8
ElisOre <elias.orellana.31@gmail.com>
2022-09-26 13:33:14 -0300

    LandingPage funcional

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './Components/Vistas/Home/Home';
 import DetailsPoke from './Components/Details/Details';
 import CreatePoke from './Components/CreatePoke/CreatePoke.jsx';
+import LandingPage from './Components/Vistas/LandingPage/LandinPage';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Switch>
-          <Route exact path='/' />
+          <Route exact path='/' component={LandingPage} />
           <Route exact path='/home' component={Home} />
           <Route exact path='/details/:id' component={DetailsPoke} />
           <Route exact path='/createPokemon' component={CreatePoke}/>
