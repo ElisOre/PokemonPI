56c9e99d2444a44c8193d5acc57f6486a3450ede
ElisOre <elias.orellana.31@gmail.com>
2022-09-27 14:44:21 -0300

    Estilos de la nav listos, solo falta ajustar el input

--- client/src/Components/Vistas/Home/Nav/NavBar.jsx
+++ client/src/Components/Vistas/Home/Nav/NavBar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { getPokeName } from "../../../../Redux/Actions";
+import './nav.css';
 
 export default function NavBar() {
     const dispatch = useDispatch();
@@ -19,8 +20,8 @@ export default function NavBar() {
     };
 
     return (
-        <div>
-            <div>
+        <div className="nav">
+            <div className="pokedex">
                 <a href="http://localhost:3000/home">
                     <img src="https://th.bing.com/th/id/R.f96a08fa96472031608960914f95beeb?rik=XZ6t%2fu9tJXTuew&riu=http%3a%2f%2f3.bp.blogspot.com%2f-_gE6akmxfrw%2fUPXpnRVzoUI%2fAAAAAAAAApQ%2feAuD868vHa4%2fs1600%2fPokedex_logo.png&ehk=N6s%2fhrMvZtCFi31HnEFV%2f1FWtbeOfj0%2bZo3tfNyV6NQ%3d&risl=&pid=ImgRaw&r=0" alt="POKÉDEX" width='150px' height='50px'/>
                 </a>
@@ -28,17 +29,17 @@ export default function NavBar() {
 
             <div>
                 <Link to='/createPokemon'>
-                    <button>CREATE A NEW POKEMON</button>
+                    <button className="buttonCreateNav">CREATE A NEW POKEMON</button>
                 </Link>
             </div>
 
-            <div>
+            <div className="navSearchName">
                 <input
                     type="search"
                     placeholder="Search for a Pokemon"
                     onChange={e => handleInputChange(e)}
                 />
-                <button type="submit" onClick={e => handleSubmit(e)}>SEARCH</button>
+                <button className="buttonSearch" type="submit" onClick={e => handleSubmit(e)}>SEARCH</button>
             </div>
         </div>
     );
