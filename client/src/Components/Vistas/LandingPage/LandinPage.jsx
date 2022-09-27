f527e5cfb4648797bde054392bf4e620773643df
ElisOre <elias.orellana.31@gmail.com>
2022-09-27 13:05:41 -0300

    Fondo de landing listo, podría mejorarse el botón

--- client/src/Components/Vistas/LandingPage/LandinPage.jsx
+++ client/src/Components/Vistas/LandingPage/LandinPage.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './landingPage.css';
 
 export default function LandingPage() {
     return (
-        <div>
-            <Link to='/home'>
-                <button>Start!</button>
-            </Link>
+        <div className="landing">
+            <div style={{ display: 'flex', flexFlow: 'column' }}>
+                <Link to='/home'>
+                    <button className="buttonLanding">Start!</button>
+                </Link>
+            </div>
         </div>
     );
 };
